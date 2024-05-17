180f75ba77c62ddd7577b3319e55bbe32378d081
indocoding-ID <indocoding.id@gmail.com>
2024-05-17 10:43:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
+# update package
+pkg update -y && pkg upgrade -y
 
+# install python3
+pkg install python3 git -y
+
+# install django
+pip install django
+
+# install python pillow
+pkg install python-pillow -y
+
+# install tzdata
+pip install tzdata
